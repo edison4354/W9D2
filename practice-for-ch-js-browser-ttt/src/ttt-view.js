db7bcf5816663a0ffe27bd2c6b3ac2e68bb5b43c
Edison <edison4354@gmail.com>
2024-04-30 12:19:13 -0700

    Add setupBoard method to View class

--- practice-for-ch-js-browser-ttt/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt/src/ttt-view.js
@@ -1,8 +1,19 @@
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
   }
   
   setupBoard() {
+    const ul = document.createElement("ul");
+    for (let i = 0; i < 3; i++){
+        for (let j = 0; j < 3; j++){
+            const li = document.createElement("li")
+            li.dataset.positions = [i, j]
+            ul.appendChild(li)
+        }
+    }
+    this.el.appendChild(ul)
   }
   
   handleClick(e) {
